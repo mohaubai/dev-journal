63fff467d4f58d0c50b9b88dc28b7c3076376a96
mohaubai <106282579+mohaubai@users.noreply.github.com>
2025-04-28 12:01:13 +0000

    Learnt the scope through nesting

--- scope_play.py
+++ scope_play.py
@@ -1,12 +1,27 @@
-from rich.console import Console
+# from rich.console import Console
 
-console = Console()
+# console = Console()
 
-message = "I am global"
+# message = "I am global"
 
-def demo():
-	message = "I am local"
-	console.print(f"inside demo [orange]locals: {locals()}")
-	console.print(f"global message still: {globals()['message']} ")
+# def demo():
+# 	message = "I am local"
+# 	console.print(f"inside demo [orange]locals: {locals()}")
+# 	console.print(f"global message still: {globals()['message']} ")
 
-demo()
+# demo()
+
+food = "pizza"
+
+def parents():
+    food = "sushi"
+    
+    def my_room():
+        #food = "burger"
+        print(f"the food in my room is {food}")
+    
+    my_room()
+    print(f"the food in my parents room is {food}")
+    
+parents()
+print(f"The food in kitchen is {food}")
